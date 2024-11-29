1e678a91e5cae60864241b2a77a5764c8d35aa44
danny8823 <dannyyoo714@gmail.com>
2024-11-29 11:34:30 -0800

    Added players api call

--- controller/nbaApiController.js
+++ controller/nbaApiController.js
@@ -16,6 +16,22 @@ const nbaAPI_Controller = {
         } catch(error) {
             console.log(error.message)
         }
+    }),
+    get_players: asyncHandler(async(req,res) => {
+        const api_url = `https://api-nba-v1.p.rapidapi.com/players`
+        const {teamId, season} = req.query
+        try {
+            const response = await axios.get(`${api_url}?team=${teamId}&season=${season}`, {
+                headers: {
+                    'x-rapidapi-key': '56b19805acmsh0e837faf42ee38ap1de4abjsnc6e3111f9c12',
+                    'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com'
+                }
+            })
+            res.status(200).json(response.data.response)
+            console.log(response.data)
+        } catch(error) {
+            console.log(error.message)
+        }
     })
 }
 
--- routes/nbaRoute.js
+++ routes/nbaRoute.js
@@ -5,5 +5,6 @@ const nbaRouter = express.Router()
 
 
 nbaRouter.get('/api/nba/teams', nbaAPI_Controller.get_team)
+nbaRouter.get('/api/nba/players', nbaAPI_Controller.get_players)
 
 module.exports = nbaRouter
